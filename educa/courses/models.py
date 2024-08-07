4cced0bc0054c4d7fde06c982a4f8a2fe60918ca
abhishek <abhishekgharami1998@gmail.com>
2024-08-07 12:31:46 +0530

    Added the module for the user .

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -38,5 +38,22 @@ class Course(models.Model):
         return self.title
     
     
+# class for the module
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ['title']
+        
+    
+    # str method
+    def __str__(self):
+        return self.title
+    
+    
     
     
