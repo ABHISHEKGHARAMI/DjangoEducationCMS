d1e1068838139f9c55edbee38932001eb91c978b
abhishek <abhishekgharami1998@gmail.com>
2024-08-07 12:17:26 +0530

    Added the subject model for the end users .

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# model for the  subject
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slugfield = models.SlugField(max_length=200,unique=True)
+    
+    # meta class for the subjects model for the ordering
+    class Meta:
+        ordering = ['title']
+        
+    #string method
+    def __str__(self):
+        return self.title
+    
