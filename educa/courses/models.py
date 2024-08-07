02b8a91f95c4e919278b68ffadae67913c124f6b
abhishek <abhishekgharami1998@gmail.com>
2024-08-07 12:24:49 +0530

    Added the model for the course.

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -16,3 +16,27 @@ class Subject(models.Model):
     def __str__(self):
         return self.title
     
+    
+# model for the courses
+class Course(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='corses_created',
+                              on_delete=models.CASCADE)
+    
+    subject = models.ForeignKey(Subject,
+                                related_name='courses',
+                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created']
+        
+    def __str__(self):
+        return self.title
+    
+    
+    
+    
