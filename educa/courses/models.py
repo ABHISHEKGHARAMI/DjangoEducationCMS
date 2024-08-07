5618bcc25e0af9d9f8c57dae95d5e3c9e08b2336
abhishek <abhishekgharami1998@gmail.com>
2024-08-07 21:20:36 +0530

    Adding the content type for the different content type.

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 
 
@@ -54,6 +56,51 @@ class Module(models.Model):
     def __str__(self):
         return self.title
     
+    
+# adding the polymorphoric  content
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type','object_id')
+    
+    
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.title
+    
+# multi table model inheritance
+class Text(ItemBase):
+    content = models.TextField()
+    
+# multi table model inheritance for the File
+class File(ItemBase):
+    content = models.FileField(upload_to='files')
+    
+# multi table model inheritance for the image
+class Image(ItemBase):
+    content = models.FileField(upload_to='images')
+    
+    
+# multi table model inheritance for video
+class Video(ItemBase):
+    url = models.URLField()
+    
+    
+
     
     
     
